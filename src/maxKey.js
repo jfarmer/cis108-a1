fef23c89ec6bdcbfdd5ce0f0548482653f5f40d2
Jason Zheng <jazheng@davidson.edu>
2020-03-24 13:53:43 -0400

    working code for maxKey.js

--- src/maxKey.js
+++ src/maxKey.js
@@ -10,11 +10,23 @@
  */
 function maxKey(obj) {
   // This is your job. :)
+  let keys = Object.keys(obj);
+  let values = Object.values(obj);
+  let initial = values[0];
+  let max = 0;;
+  for (let i = 1; i < values.length; i++) {
+    if (initial < values[i]) {
+      initial = values[i]
+      max = i;
+    }
+  }
+  return keys[max];
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for maxKey:');
-
+  console.log('The max key of the object is:', maxKey({a:1, b:2, c:3}));
+  console.log('The max key of the object is:', maxKey({a:100, b:-4, c:0, d:40}));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
