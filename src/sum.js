481a39c668208651050fd1f55ab1bbce8fab91c6
Jason Zheng <jazheng@davidson.edu>
2020-03-24 13:53:28 -0400

    working code for sum.js

--- src/sum.js
+++ src/sum.js
@@ -7,6 +7,14 @@
  */
 function sum(array) {
   // This is your job. :)
+  if (array.length === 0) {
+    return 0;
+  }
+  let count = 0;
+  for (let element of array) {
+    count += element;
+  }
+  return count;
 }
 
 if (require.main === module) {
@@ -14,6 +22,9 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log('The sum of the array is:', sum([]));
+  console.log('The sum of the array is:', sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+  console.log('The sum of the array is:', sum([-10, 0, -20]));
 }
 
 module.exports = sum;
