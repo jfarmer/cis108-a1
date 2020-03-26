dce34a073dae67c4d937d7384c9f47a8ddb75864
Jalen Staples <jastaples@davidson.edu>
2020-03-26 02:16:35 -0400

    Completed all except maxKey.  Still trying t figure it out.

--- src/avg.js
+++ src/avg.js
@@ -10,14 +10,23 @@ let sum = require('./sum');
  * @returns {number} The average
  */
 function avg(array) {
+  let count=0;
+  for (let i in array) {
+
+    count+=array[i] }
+
+    return count/array.length
+  }
   // This is your job. :)
   // Use the sum function you wrote rather than re-implementing
   // it in this file. We required it above.
-}
+
 
 if (require.main === module) {
   console.log('Running sanity checks for avg:');
-
+console.log(avg([1,2,3,4,5]));
+console.log(avg([5,5,5,5,5]));
+console.log(avg([5,10,15,20,25]));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
--- src/sum.js
+++ src/sum.js
@@ -6,13 +6,20 @@
  * @returns {number} The sum
  */
 function sum(array) {
+
+  let count=0;
+ for (let i in array) {
+  count+=array[i] }
+  return count;
   // This is your job. :)
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for sum:');
-
-  // Add your own sanity checks here.
+console.log('Running sanity checks for sum:');
+console.log(sum([5,5,5,5,5]));
+console.log(sum([8,6,7,4]));
+console.log(sum([25,25,25,25,25]));
+// Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
 
