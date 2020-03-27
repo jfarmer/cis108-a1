9f046d54e39e195b5e5328be7f35660397f04667
Kendahl Ross <krross02@gmail.com>
2020-03-27 17:54:21 -0500

    KendahlcisA1

--- src/max.js
+++ src/max.js
@@ -6,13 +6,23 @@
  * @returns {number} The largest number
  */
 function max(array) {
-  // This is your job. :)
-  // Don't use the built-in Math.max
+let maxSoFar = array[0];
+
+for (let element of array) {
+if (element > maxSoFar) {
+maxSoFar = element;
+}
+}
+return maxSoFar;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for max:');
-
+console.log('----------');
+console.log(max([2,4,8,7,5,22,87,11,44,12]));
+console.log(`Let's make sure that wasn't a fluke`);
+console.log('----------');
+console.log(max([34,21,48,17,18,25,8]));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
